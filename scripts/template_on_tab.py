681d78090b27bb0511a35af65695f3eb2485255c
levieli <levieli@tencent.com>
2023-07-07 16:47:55 +0800

    init demo

--- scripts/template_on_tab.py
+++ scripts/template_on_tab.py
@@ -4,15 +4,23 @@ from modules.shared import opts
 from modules import extensions
 
 # Handy constants
-PHOTOPEA_MAIN_URL = "https://www.apeg.cn/teleprompter?source=webui"
-PHOTOPEA_IFRAME_ID = "webui-apeg-iframe"
+PHOTOPEA_MAIN_URL = "https://https://apeg.cn/teleprompter?source=webui"
+PHOTOPEA_IFRAME_ID = "webui-photopea-iframe"
 PHOTOPEA_IFRAME_HEIGHT = 768
 PHOTOPEA_IFRAME_WIDTH = "100%"
+PHOTOPEA_IFRAME_LOADED_EVENT = "onPhotopeaLoaded"
 
 
 # Adds the "Apeg" tab to the WebUI
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as apeg_tab:
+        # Check if Controlnet is installed and enabled in settings, so we can show or hide the "Send to Controlnet" buttons.
+        controlnet_exists = False
+        for extension in extensions.active():
+            if "controlnet" in extension.name:
+                controlnet_exists = True
+                break
+
         with gr.Row():
             # Add an iframe with Photopea directly in the tab.
             gr.HTML(
